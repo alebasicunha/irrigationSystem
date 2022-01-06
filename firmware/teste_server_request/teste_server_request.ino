37e073b5b99275e14b4fc8b96e737ab9f305c004
alebasicunha <bela.alebasi.cunha@gmail.com>
2022-01-05 22:21:57 -0300

    Firmware: implementa estrutura de dados e conversão para json.

--- firmware/teste_server_request/teste_server_request.ino
+++ firmware/teste_server_request/teste_server_request.ino
@@ -3,33 +3,32 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-#define TIMEOUT  5000  
-#define PERIODO_DEFAULT 1;  
-#define LIMITE_MINIMO 50;  
-#define LIMITE_MAXIMO 80;  
-
-int macAddr;
-int periodoMedicao = PERIODO_DEFAULT; 
-int limiteMinimo = LIMITE_MINIMO;
-int limiteMaximo = LIMITE_MAXIMO;
-float umidadeAtual;
-
-struct dadosDoSistema {
-  String macAddr;
-  int periodoMedicao = PERIODO_DEFAULT; 
-  int limiteMinimo = LIMITE_MINIMO;
-  int limiteMaximo = LIMITE_MAXIMO;
-  float umidadeAtual;
-};
+#define TIMEOUT 5000
+#define PORTA_DEFAULT 80  
+#define PERIODO_DEFAULT 1
+#define LIMITE_MINIMO 50  
+#define LIMITE_MAXIMO 80  
 
 //login e senha do wi-fi
-const char *ssid = "Sei la";         // replace with your wifi ssid and wpa2 key
+const char *ssid = "Sei la";         
 const char *pass = "eujadisse";
 
+//variaveis de comunicacao
 String serverName = "http://192.168.15.46:8080/api/v1/systems";
 WiFiClient client;
 HTTPClient http;
-ESP8266WebServer server(80);
+ESP8266WebServer server(PORTA_DEFAULT);
+
+struct DadosDoSistema {
+  String macAddr;
+  int porta;
+  int periodoMedicao; 
+  int limiteMinimo;
+  int limiteMaximo;
+  float umidadeAtual;
+};
+
+DadosDoSistema dadosDoSistema;
 
 void setup()
 {
@@ -51,10 +50,14 @@ void setup()
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP()); //IP local do ESP8266 na rede wifi
 
-  server.on("/atualizarDados", atualizarDados); //Especifica qual funcao sera chamada qdo recebermos um request
- 
-  server.begin(); //Start the server
+  //Especifica qual funcao sera chamada qdo recebermos um request
+  server.on("/atualizarDados", atualizarDados); 
+
+  //Inicia servidor esp8266
+  server.begin(); 
   Serial.println("Server listening");
+
+  inicializaEstruturaDados();
 }
 
 void loop()
@@ -62,6 +65,19 @@ void loop()
   server.handleClient(); //Handling of incoming requests
   // 1 sec delay
   delay(1000);
+}
+
+void inicializaEstruturaDados() {
+  float h = lerSensor();
+  
+  dadosDoSistema = {
+    WiFi.macAddress(),
+    80,
+    PERIODO_DEFAULT,
+    LIMITE_MINIMO,
+    LIMITE_MAXIMO,
+    h
+  };
 }
 
 float lerSensor() {
@@ -77,10 +93,12 @@ float lerSensor() {
   
   Serial.print("Humidity Level: ");
   Serial.println(h);
+  dadosDoSistema.umidadeAtual = h;
   return h;
 }
 
-void atualizarDados() { //Handler para o atualizarDados
+//Handler para o atualizarDados
+void atualizarDados() { 
  
   //if (server.hasArg("plain")== false){ //Check if body received
   //      server.send(200, "text/plain", "Body not received");
@@ -92,8 +110,23 @@ void atualizarDados() { //Handler para o atualizarDados
   //       message += "\n";
   //Serial.println(message);
 
-  String resposta = "{\"umidade\":\"" + String(lerSensor(), 2) + "\"}";
+  lerSensor();
+  String resposta = toJson();//"{\"umidade\":\"" + String(lerSensor(), 2) + "\"}";
 
   server.send(200, "text/plain", resposta);
   Serial.println(resposta);
+}
+
+String toJson() {
+  String abreJson = "{";
+  String mac = "\"macAddress\":\"" + dadosDoSistema.macAddr + "\"";
+  String porta = "\"porta\":\"" + String(dadosDoSistema.porta) + "\"";
+  String periodoMedicao = "\"periodoMedicao\":\"" + String(dadosDoSistema.periodoMedicao) + "\"";
+  String limiteMinimo = "\"limiteMinimo\":\"" + String(dadosDoSistema.limiteMinimo) + "\"";
+  String limiteMaximo = "\"limiteMaximo\":\"" + String(dadosDoSistema.limiteMaximo) + "\"";
+  String umidade = "\"umidade\":\"" + String(dadosDoSistema.umidadeAtual, 2) + "\"";
+  String fechaJson = "}";
+  String v = ",";
+
+  return abreJson + mac + v + porta + v + periodoMedicao + v + limiteMinimo + v + limiteMaximo + v + umidade + fechaJson; 
 }
